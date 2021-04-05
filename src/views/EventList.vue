f1184b18ff661f3d327f1c756c8cb91722b4a8f2
GayofReckoning <afox09@gmail.com>
2021-04-05 16:01:47 -0500

    end L5

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -10,6 +10,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
 
 export default {
   name: "EventList",
@@ -18,42 +19,18 @@ export default {
   },
   data(){
     return{
-      events:[
-        {
-        id: 5928101,
-        category: 'animal welfare',
-        title: 'Cat Adoption Day',
-        description: 'Find your new feline friend at this event!',
-        location: 'Meow Town',
-        date: 'January 28, 2022',
-        time: '12:00',
-        petsAllowed: true,
-        organizer: 'Kat Laydee'
-      },
-       {
-        id: 5923241,
-        category: 'food',
-        title:'Community Gardening',
-        description: 'Join us as we tend to teh community edible plants.',
-        location: 'Flora City',
-        date: 'March 14, 2022',
-        time: '10:00',
-        petsAllowed: true,
-        organizer: 'Fern Pollin'
-      },
-       {
-        id: 5524261,
-        category: 'sustainability',
-        title: 'Beach Cleanup',
-        description: 'Help us pick up trash along the shore.',
-        location: 'Playa Del Carmen',
-        date: 'July 22, 2022',
-        time: '11:00',
-        petsAllowed: false,
-        organizer: 'Carey Wales'
-      }
-      ]
+      events: null
     }
+  },
+  created(){
+    EventService.getEvents()
+    .then((response) => {
+      this.events = response.data;
+      })
+    .catch(error => {
+      alert(`oh no server request failed! Check the console for details!`)
+      console.log('error getting events: ', error)
+    })
   }
 };
 </script>
