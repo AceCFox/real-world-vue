ff24cb4954ccb1a33546b63701414c0516d9b4f6
GayofReckoning <afox09@gmail.com>
2021-04-06 12:19:49 -0500

    working 3/4 stack

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
 import { createStore } from "vuex";
+import EventService from '@/services/EventService.js';
 
 export default createStore({
   state: {
     user: {id: 'abc123', name: 'Adam Jahr' },
-    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community']
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state,event){
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event)
+      commit ('ADD_EVENT', event)
+      console.log('in createEvent action')
+    } 
+  },
   getters: {
     catLength: state => {
       return state.categories.length
